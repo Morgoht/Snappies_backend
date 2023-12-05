55055a5d34bcf3e942060833dfe4e5f89e31e566
maximeVa <maxime.vanachter@student.vinci.be>
2023-12-05 11:47:04 +0100

    Project setup

---
+++ src/main/java/com/example/Snappies_backend/SnappiesBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.Snappies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnappiesBackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/Snappies_backend/SnappiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Snappies_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
