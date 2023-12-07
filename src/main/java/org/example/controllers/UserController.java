8d13fbb96fef970cf81eed62057cb1f2533d9859
Emilz <emil.zerun@student.vinci.be>
2023-12-07 10:02:10 +0100

    CRUD User methods

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -3,6 +3,7 @@ package org.example.controllers;
 import org.example.services.UserService;
 import org.example.models.User;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @Controller
@@ -23,12 +26,48 @@ public class UserController {
 
     @QueryMapping
     public User userById(@Argument String userId) throws ExecutionException, InterruptedException {
-        System.out.println(userId);
-        return service.getUser(userId);
+        return service.userById(userId);
+    }
+
+    @QueryMapping
+    public List<User> allUsers() throws ExecutionException, InterruptedException {
+        return service.allUsers();
     }
 
 
-    @GetMapping("/test")
+    @MutationMapping
+    public User  createUser(@Argument String name, @Argument String lastname,
+                           @Argument String username, @Argument String email, @Argument String password) throws ExecutionException, InterruptedException {
+        User user = new User();
+        user.setDocument_id(UUID.randomUUID().toString());
+        user.setName(name);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        service.createUser(user);
+        return user;
+    }
+
+
+    @MutationMapping
+    public User updateUser(@Argument String name, @Argument String lastname,
+                            @Argument String username, @Argument String email, @Argument String password) throws ExecutionException, InterruptedException {
+        User user = new User();
+        user.setDocument_id(UUID.randomUUID().toString());
+        user.setName(name);
+        user.setLastname(lastname);
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        service.createUser(user);
+        return user;
+
+    }
+
+
+
+        @GetMapping("/test")
     public ResponseEntity<String> testGetEndpoint(){
         return ResponseEntity.ok("Test Endpoint Get is working");
     }
--- src/main/java/org/example/services/UserService.java
+++ src/main/java/org/example/services/UserService.java
@@ -1,13 +1,13 @@
 package org.example.services;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
-import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import org.example.models.User;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -16,7 +16,7 @@ public class UserService {
 
     Firestore dbFirestore = FirestoreClient.getFirestore();
 
-    public User getUser(String documentId) throws ExecutionException, InterruptedException {
+    public User userById(String documentId) throws ExecutionException, InterruptedException {
         DocumentReference documentReference = dbFirestore.collection("users").document(documentId);
         ApiFuture<DocumentSnapshot> future = documentReference.get();
         DocumentSnapshot document = future.get();
@@ -25,7 +25,34 @@ public class UserService {
             user = document.toObject(User.class);
             return user;
         }
-
         return null;
+    }
+
+    public List<User> allUsers() throws ExecutionException, InterruptedException {
+        CollectionReference collection = dbFirestore.collection("users");
+        ApiFuture<QuerySnapshot> querySnapshot = collection.get();
+
+        List<User> userList = new ArrayList<>();
+        for (DocumentSnapshot doc : querySnapshot.get().getDocuments()) {
+            User user = doc.toObject(User.class);
+            userList.add(user);
+        }
+
+        return userList;
+    }
+
+    public String createUser(User user) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getDocument_id()).set(user);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String updateUser(User user) throws ExecutionException, InterruptedException {
+        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("users").document(user.getDocument_id()).set(user);
+        return collectionsApiFuture.get().getUpdateTime().toString();
+    }
+
+    public String deleteUser(String documentId){
+        ApiFuture<WriteResult> writeResultApiFuture = dbFirestore.collection("users").document(documentId).delete();
+        return "Successfully deleted user";
     }
 }
